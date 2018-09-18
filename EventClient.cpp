74818709389693dd33824e801c76632d4b4150e4
He Dengfeng <hedengfeng@lingtrack.com>
2018-09-18 09:43:02 +0800

    project setup file commit

--- EventClient.cpp
+++ EventClient.cpp
@@ -106,8 +106,7 @@ bool EventClient::unregister_observer(Message id, const EventTarget &object)
 ******************************************************************************/
 EventClient::EventClient()
 {
-	// 先初始化成员，再注册到MsgCenter。这是因为如果先注册会出现某个线程在构造的时候，
-	// 就有消息传到该线程。EventClient::add_message_to_receive_queue被异步调用，在此
+	// 先初始化成员，再注册到EventBus。这是因为如果先注册会出现某个线程在构造的时候，
 	// 方法中会操作_msg_object_map，因此在这里必须先初始化成员。
 	for(unsigned int i=0; i<EVENT_NR; i++)
 	{
@@ -118,36 +117,36 @@ EventClient::EventClient()
 		}
 	}
 
-	EventBus *msg_center = EventSingleton::instance();
+	EventBus *event_bus = EventSingleton::instance();
 
-//	_zmq_context = msg_center->getZmqContext();
-//
-//	thread::id self_id= this_thread::get_id();
-//	printf("self_id = %p,%d, _zmq_context = %p \n",self_id,self_id,_zmq_context);
-//
-//
-//	//创建zmq的套接字，设置套接字为请求应答模式
-//	_sub_socket = zmq_socket(_zmq_context, ZMQ_SUB);
-//	assert(_sub_socket);
-//
-//	int rc = zmq_connect(_sub_socket, EventBus::MESSAGE_CENTER_ENDPOINT);
-//	assert(rc == 0);
-//
-//	rc = zmq_setsockopt(_sub_socket, ZMQ_SUBSCRIBE, "boy", 3);
-//	assert(rc == 0);
-//	rc = zmq_setsockopt(_sub_socket, ZMQ_SUBSCRIBE, "girl", 4);
-//	assert(rc == 0);
-//
-//	_pub_socket = zmq_socket(_zmq_context,ZMQ_PUB);
-//	assert(_pub_socket);
-//
-//	rc = zmq_bind(_pub_socket, EventBus::MESSAGE_CENTER_ENDPOINT);
-//	assert(rc == 0);
+	_zmq_context = event_bus->getZmqContext();
+
+	thread::id self_id= this_thread::get_id();
+	printf("self_id = %p,%d, _zmq_context = %p \n",self_id,self_id,_zmq_context);
 
 
-	if(msg_center->register_client(this_thread::get_id(), *this) == false)
+	//创建zmq的套接字，设置套接字为请求应答模式
+	_sub_socket = zmq_socket(_zmq_context, ZMQ_SUB);
+	assert(_sub_socket);
+
+	int rc = zmq_connect(_sub_socket, EventBus::MESSAGE_CENTER_ENDPOINT);
+	assert(rc == 0);
+
+	rc = zmq_setsockopt(_sub_socket, ZMQ_SUBSCRIBE, "boy", 3);
+	assert(rc == 0);
+	rc = zmq_setsockopt(_sub_socket, ZMQ_SUBSCRIBE, "girl", 4);
+	assert(rc == 0);
+
+	_pub_socket = zmq_socket(_zmq_context,ZMQ_PUB);
+	assert(_pub_socket);
+
+	rc = zmq_bind(_pub_socket, EventBus::MESSAGE_CENTER_ENDPOINT);
+	assert(rc == 0);
+
+
+	if(!event_bus->register_client(this_thread::get_id(), *this))
 	{
-		printf("Register client to message center is failed!\n");
+		printf("Register client to event bus is failed!\n");
 	}
 }
 
